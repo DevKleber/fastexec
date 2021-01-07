e78e8cd1949a95a259d0fe5877abfcadc93a4cd6
Kleber de Souza <kleber107@gmail.com>
2021-01-07 17:52:13 -0300

    initial

---
+++ fastexec.py
@@ -0,0 +1,114 @@
+from pynput.keyboard import Listener
+import platform
+import getpass
+import os
+from shutil import copyfile
+import shutil
+import webbrowser
+import sys
+
+userName = getpass.getuser()
+system = platform.system()
+
+parentDirWindows = ""
+parentDirLinux = "/home/"+userName+"/.config/"
+directory="fastexec"
+nameFile="fastexec.py"
+nameCompile="fastexec.py"
+
+sentence = ""
+version = "Version=1.5"
+
+def checkVersion():
+    fileExec = "/home/"+userName+"/.config/autostart/fastexec.desktop"
+    path = os.path.join(parentDirLinux, directory) 
+    if os.path.isfile(fileExec):
+        config = open(fileExec, 'r').read()
+        arConfig = config.split("\n")
+        sameVersion = version in arConfig
+        if(sameVersion == False):
+            os.remove(fileExec)
+            shutil.rmtree(path)
+
+def operatingSystem():
+    checkVersion()
+    path = os.path.join(parentDirLinux, directory) 
+    if(verifyFileExists(path) == False):
+        os.mkdir(path)
+        copyfile(nameFile, path+"/"+nameCompile)
+        os.remove(nameFile)
+    
+    reboot(system)
+
+def verifyFileExists(systemName):
+    pathFile = os.path.join(parentDirLinux, directory,nameCompile)
+    if os.path.isfile(pathFile):
+        return True
+    
+    return False
+
+def reboot(systemName):
+    fileExec = "/home/"+userName+"/.config/autostart/fastexec.desktop"
+    if os.path.isfile(fileExec) == False:
+        pathFile = os.path.join(parentDirLinux, directory,nameCompile)
+        textFile ="[Desktop Entry]\nType=Application\n"+version+"\nExec=python3 "+pathFile+"\nX-GNOME-Autostart-enabled=true\nNoDisplay=false\nHidden=false\nName[en_US]=fastexec\nName[pt_BR]=fastexec\nComment[en_US]=No description\nX-GNOME-Autostart-Delay=1"
+
+        f = open(fileExec, "a")
+        f.write(textFile)
+        f.close()
+
+operatingSystem()
+
+def clearSentence():
+    global sentence
+    sentence = ""
+
+def fastExec(key):
+    global sentence
+    keydata = str(key)    
+    # python == 2
+    if(sys.hexversion == 34017776): 
+        keyWithOutQuotationMarks = str(key).replace("'", "")    
+        key = keyWithOutQuotationMarks.replace('u', '', 1)
+    else:
+        key =str(key).replace("'", "")
+
+    split = keydata.split('.')
+    size = len(split)
+    
+    if(size == 2):
+        clearSentence()
+    else:
+        sentence = sentence+key
+
+    print(sentence)
+    if(len(sentence) >= 100):
+        sentence =""
+    
+    if(sentence == 'opsigaderr'):
+        webbrowser.get('firefox').open_new_tab('sigaderr.aderr.com.br')
+        sentence =""
+    if(sentence == 'opsidap'):
+        webbrowser.get('firefox').open_new_tab('http://sidap.sedap.pb.gov.br')
+        sentence =""
+    if(sentence == 'oppecus'):
+        webbrowser.get('firefox').open_new_tab('http://pecus.ipeweb.com.br/password')
+        sentence =""
+    if(sentence == 'opjira'):
+        webbrowser.get('firefox').open_new_tab('sigaderr.aderr.com.br:8080')
+        sentence =""
+    if(sentence == 'opbitbucket'):
+        webbrowser.get('firefox').open_new_tab('sigaderr.aderr.com.br:7990')
+        sentence =""
+    if(sentence == 'opgales'):
+        webbrowser.get('firefox').open_new_tab('http://jira.sidap.sedap.pb.gov.br')
+        sentence =""
+    if(sentence == 'opsidaam'):
+        webbrowser.get('firefox').open_new_tab('http://sidaam.adaf.am.gov.br')
+        sentence =""
+    if(sentence == 'opyoutube'):
+        webbrowser.get('firefox').open_new_tab('https://www.youtube.com')
+        sentence =""
+
+with Listener(on_press=fastExec) as listener:
+    listener.join()
